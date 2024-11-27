e71db192f58e0f6fcd0705781324e704e58442d7
BenBraS <benjamin.br.sa@gmail.com>
2024-11-27 14:20:34 -0300

    Revert "Refactor mocks.service 1 solo archivo"
    This reverts commit 8e2be985b01b5d7160704a6a90275abc97be5c42.

--- src/controllers/mocks.controller.js
+++ src/controllers/mocks.controller.js
@@ -1,19 +1,19 @@
-import { generateMockDataService } from '../services/mocks.service.js';
+import { generateMockDataService } from '../services/generateMockData.service.js';
 
 export const generateMockData = async (req, res) => {
   const { users, pets } = req.body;
-  console.log('Datos recibidos:', users, pets);
 
+  // Validación de parámetros
   if (!users || !pets || isNaN(users) || isNaN(pets) || users <= 0 || pets <= 0) {
     return res.status(400).json({ error: 'Se deben indicar valores válidos y positivos para usuarios y mascotas.' });
   }
 
   try {
-    console.log('Generando datos de mock...');
+    // Llama al servicio para generar y guardar los datos
     await generateMockDataService(users, pets);
     res.json({ message: `Datos generados: ${users} usuarios y ${pets} mascotas.` });
   } catch (error) {
-    console.error('Error al generar los datos:', error);
+    console.error('Error al generar los datos:', error); // Para depuración
     res.status(500).json({ error: 'Error al generar los datos.', details: error.message });
   }
 };
--- src/services/mocks.service.js
+++ src/services/mocks.service.js
@@ -1,16 +1,14 @@
 import { faker } from '@faker-js/faker';
 import bcrypt from 'bcrypt';
-import { usersService } from '../services/index.js';  // Ajusta la ruta según la ubicación de tu archivo
-import { petsService } from '../services/index.js';   // Ajusta la ruta según la ubicación de tu archivo
 
-// Función para generar usuarios
+// Función para crear usuarios
 export const generateUsers = async (count) => {
   const users = [];
 
   for (let i = 0; i < count; i++) {
     const role = Math.random() > 0.5 ? 'admin' : 'user'; 
-
-    // Contraseña encriptada
+    
+    //contraseña encriptada
     const passwordHash = await bcrypt.hash('coder123', 10);
 
     users.push({
@@ -26,7 +24,7 @@ export const generateUsers = async (count) => {
   return users;
 };
 
-// Función para generar mascotas
+
 export const generatePets = (count) => {
   const pets = [];
 
@@ -39,20 +37,4 @@ export const generatePets = (count) => {
   }
 
   return pets;
-};
-
-// Servicio para generar y guardar datos (usuarios y mascotas) en la base de datos
-export const generateMockDataService = async (userCount, petCount) => {
-  try {
-    // Generar usuarios y mascotas
-    const users = await generateUsers(userCount); 
-    const pets = generatePets(petCount); 
-
-    // Guardar los usuarios y mascotas en la base de datos
-    await usersService.saveMany(users);
-    await petsService.saveMany(pets);
-  } catch (error) {
-    console.error('Error al generar los datos:', error);
-    throw new Error('Error al generar los datos de mock');
-  }
 };
