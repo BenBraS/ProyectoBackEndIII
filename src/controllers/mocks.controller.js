8e2be985b01b5d7160704a6a90275abc97be5c42
BenBraS <benjamin.br.sa@gmail.com>
2024-11-27 14:18:38 -0300

    Refactor mocks.service 1 solo archivo

--- src/controllers/mocks.controller.js
+++ src/controllers/mocks.controller.js
@@ -1,19 +1,19 @@
-import { generateMockDataService } from '../services/generateMockData.service.js';
+import { generateMockDataService } from '../services/mocks.service.js';
 
 export const generateMockData = async (req, res) => {
   const { users, pets } = req.body;
+  console.log('Datos recibidos:', users, pets);
 
-  // Validación de parámetros
   if (!users || !pets || isNaN(users) || isNaN(pets) || users <= 0 || pets <= 0) {
     return res.status(400).json({ error: 'Se deben indicar valores válidos y positivos para usuarios y mascotas.' });
   }
 
   try {
-    // Llama al servicio para generar y guardar los datos
+    console.log('Generando datos de mock...');
     await generateMockDataService(users, pets);
     res.json({ message: `Datos generados: ${users} usuarios y ${pets} mascotas.` });
   } catch (error) {
-    console.error('Error al generar los datos:', error); // Para depuración
+    console.error('Error al generar los datos:', error);
     res.status(500).json({ error: 'Error al generar los datos.', details: error.message });
   }
 };
